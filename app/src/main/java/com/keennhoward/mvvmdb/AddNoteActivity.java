0569b9c18f12ca210a3764fc83b95bc57dab0cc2
Kino <keennhc@gmail.com>
2020-12-07 01:57:19 +0800

    Add Notes to DB

--- app/src/main/java/com/keennhoward/mvvmdb/AddNoteActivity.java
+++ app/src/main/java/com/keennhoward/mvvmdb/AddNoteActivity.java
@@ -1,14 +1,101 @@
 package com.keennhoward.mvvmdb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
+    //psfs for auto complete public static final String
+    //constants
+    //best practice for intent Extra keys is to use the package name to keep them unique
+    public static final String EXTRA_TITLE =
+            "com.keennhoward.mvvmdb.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "com.keennhoward.mvvmdb.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY=
+            "com.keennhoward.mvvmdb.EXTRA_PRIORITY";
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        //set Max min of number picker
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMaxValue(10);
+
+        //to get X on top left corner
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note"); //set action bar title
+    }
+
+    private void saveNote(){
+        //get input from fields
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        int priority = numberPickerPriority.getValue();
+
+        //check if editTexts are empty
+        //trim removes spaces
+        if(title.trim().isEmpty() || description.trim().isEmpty()){
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return; //leaves the method (does not execute anything below it)
+        }
+        //best practice is to make 2 separate viewModel classes
+        //but we will just send data back to the main activity using startActivityForResult
+        //this activity just works like an input form and does not communicate with any other layout
+
+        //create intent and put extra
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title); //(KEY, VALUE)
+        data.putExtra(EXTRA_DESCRIPTION,description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        //to indicate if the input was successful or not
+        setResult(RESULT_OK, data);
+
+        finish();// close activity
+    }
+
+    //add Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //must be above return
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu); //use add_note_menu as the ,emu in this activity
+
+        //must return true to display the menu false if not
+        return true;
+    }
+
+    //to handle clicks on menu icons
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) { //to find out which one was clicked
+            case R.id.save_note:
+                saveNote();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/keennhoward/mvvmdb/MainActivity.java
+++ app/src/main/java/com/keennhoward/mvvmdb/MainActivity.java
@@ -1,17 +1,25 @@
 package com.keennhoward.mvvmdb;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //put requests code in constants so that it is easily distinguished
+    public static final int ADD_NOTE_REQUEST = 1;
 
     private NoteViewModel noteViewModel;
 
@@ -19,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Floating Action Button
+        FloatingActionButton buttonAddNote = findViewById(R.id.button_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //use MainActivity.this instead of just this because it will point to the onClickListener
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST); //to get input back (Request Code is used to distinguish between requests)
+            }
+        });
 
         //setup recyclerview after NoteAdapter Class
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
@@ -43,4 +62,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //get result back from AddNoteActivity
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //check which request we are handling && if resultCode is RESULT_OK
+        if(requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK){
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            //create a note from all the data to insert it into the database
+            Note note = new Note(title, description,priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
+        }
+        //this will also be triggered over the back button in the other activity(RESULT_CANCELLED)
+        //add android:parentActivityName=".MainActivity" to the activity in the manifest you want a back button to show
+        //also add android:launchMode="singleTop" on the main activity in the manifest so that once it goes back it will not trigger on create again and just go back to the previous one
+        else{
+            Toast.makeText(this, "Note not Saved", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
